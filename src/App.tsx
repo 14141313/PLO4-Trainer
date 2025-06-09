34da89946d14acf9442070b089432d64800f8376
14141313 <14141313ds@gmail.com>
2025-06-10 08:40:02 +1000

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="p-8 text-center text-2xl font-bold text-white bg-black min-h-screen">
-      ✅ PLO4 Trainer MVP is Working!
-    </div>
-  );
-}
